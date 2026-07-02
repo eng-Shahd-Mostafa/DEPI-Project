a9b771ce9c57c412dda5fe7585b9eadea2fdd756
Shahd Mostafa <eng.shahd.mostafa@gmail.com>
2026-07-02 14:01:02 +0300

    edit on app.py

--- app.py
+++ app.py
@@ -113,5 +113,7 @@ def predict():
             'error': str(e)
         })
 
+# if __name__ == '__main__':
+#     app.run(debug=True, host='0.0.0.0', port=5000)
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=7860)
